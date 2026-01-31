51fc009ad5114596dd89e3ab85a4092cc5d7c880
Kiri <cultivatingghoul@gmail.com>
2026-01-31 10:04:32 +0200

    removed unwanted package and styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import assetDropLogo from '@/assets/LightIcon.svg';
 
 const Header = () => {
     return (
-        <div className='flex items-center gap-2 bg-light-fill p-2 border-light-fill border-b'>
+        <div className='flex items-center gap-2 bg-light-fill p-2 border-gray-500 border-b'>
             <div className='mt-1'>
                 <img src={assetDropLogo} className="logo" alt="Asset Drop logo" />
             </div>
